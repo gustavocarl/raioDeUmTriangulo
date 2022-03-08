d812c1e8679b402c716ccea919e8463f46d8d4b9
Gustavo Henrique Carl Dos Santos <gustavo_carl@live.com>
2022-03-07 21:45:35 -0300

    Adicionar arquivos de projeto.

---
+++ raioDeUmTriangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace raioDeUmTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float raio, area;
+            Console.WriteLine("Informe o raio do circulo");
+            raio = float.Parse(Console.ReadLine());
+            area = (float)(3.14 * (raio * raio));
+            Console.WriteLine("A área do circulo é: {0}", area);
+        }
+    }
+}
